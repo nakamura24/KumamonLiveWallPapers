05b527b49730f004b2eff6d15385c96a6b1b3e42
M.Nakamura <zmf42190@gmail.com>
2013-01-02 10:12:39 +0900

    1.1.12

---
+++ src/jp/kumamonlivewallpapers/KumamonLockLiveWallPaper.java
@@ -0,0 +1,160 @@
+﻿/*
+ * Copyright (C) 2012 M.Nakamura
+ *
+ * This software is licensed under a Creative Commons
+ * Attribution-NonCommercial-ShareAlike 2.1 Japan License.
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 		http://creativecommons.org/licenses/by-nc-sa/2.1/jp/legalcode
+ */
+
+package jp.kumamonlivewallpapers;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+import java.util.Random;
+
+import jp.kumamonlivewallpapers.ForecastTask;
+import jp.kumamonlivewallpapers.R;
+import jp.template.LiveWallPaper;
+import android.content.SharedPreferences;
+import android.content.res.Resources;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Typeface;
+import android.preference.PreferenceManager;
+import android.view.Display;
+import android.view.WindowManager;
+
+public class KumamonLockLiveWallPaper extends LiveWallPaper {
+	private int displayWidth;
+	private Random randam = new Random();
+	private boolean warking = false;
+	private int preSingleTap = 0;
+	private int mLocateId = 0;
+
+	@Override
+	public void onCreate() {
+		super.onCreate();
+		// ウィンドウマネージャのインスタンス取得
+		WindowManager windowManager = (WindowManager)getSystemService(WINDOW_SERVICE);
+		// ディスプレイのインスタンス生成
+		Display display = windowManager.getDefaultDisplay();
+		displayWidth = display.getWidth();
+		BackgroundColor = Color.WHITE;
+	}
+
+	@Override
+	public void onDestroy() {
+		super.onDestroy();
+	}
+
+	@Override
+	public Engine onCreateEngine() {
+		return new LiveEngine();
+	}
+
+	@Override
+	public void DrawCanvas(Canvas canvas) {
+		super.DrawCanvas(canvas);
+		// draw something
+		OverLayer(canvas);
+		KumamonCopyright(canvas);
+	}
+
+	@Override
+	public void ChangeImage() {
+		if(preSingleTap != SingleTap) {
+			int[] images = {R.drawable.lock2,R.drawable.lock3,R.drawable.lock4,R.drawable.lock5,};
+			Image = BitmapFactory.decodeResource(getResources(), images[randam.nextInt(images.length)]);
+			preSingleTap = SingleTap;
+			DelayMillis = 3000;	// millisecond
+		} else {
+			if(warking) {
+				Image = BitmapFactory.decodeResource(getResources(), R.drawable.lock0);
+				warking  = false;
+			} else {
+				Image = BitmapFactory.decodeResource(getResources(), R.drawable.lock1);
+				warking  = true;
+			}
+			DelayMillis = 1000;	// millisecond
+		}
+		getForecast();
+	}
+
+	private void OverLayer(Canvas canvas) {
+		SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+		Paint paint = new Paint();
+		paint.setColor(Color.BLACK);
+		paint.setTypeface(Typeface.DEFAULT_BOLD);
+		if(sharedPreferences.getBoolean("date", false)) {
+			paint.setTextSize(18);
+			Date date = Calendar.getInstance().getTime();
+			SimpleDateFormat sdf1 = new SimpleDateFormat("yyyy/MM/dd(EEE)", Locale.JAPANESE);
+			SimpleDateFormat sdf2 = new SimpleDateFormat("HH:mm", Locale.JAPANESE);
+			canvas.drawText(sdf1.format(date), 340, 70, paint);
+			paint.setTextSize(50);
+			canvas.drawText(sdf2.format(date), 340, 115, paint);
+			int battery = (int)((double)BatteryLevel / BatteryScale * 100.0 + 0.5);
+			paint.setTextSize(18);
+			Resources resource = getResources();
+			canvas.drawText(resource.getString(R.string.battery)+ String.valueOf(battery) +"%", 340, 140, paint);
+		}
+
+		if(sharedPreferences.getBoolean("forecast", false)) {
+			String today = sharedPreferences.getString(ForecastTask.KEY_TODAY, "");
+			String tomorrow = sharedPreferences.getString(ForecastTask.KEY_TOMORROW, "");
+			String day_after_tomorrow = sharedPreferences.getString(ForecastTask.KEY_DAY_AFTER_TOMORROW, "");
+			paint.setTextSize(24);
+			if(Calendar.getInstance().get(Calendar.HOUR_OF_DAY) >= 18) {
+				canvas.drawText(tomorrow, 10, 70, paint);
+				canvas.drawText(day_after_tomorrow, 10, 100, paint);
+			} else {
+				canvas.drawText(today, 10, 70, paint);
+				canvas.drawText(tomorrow, 10, 100, paint);
+			}
+		}
+	}
+
+	private void KumamonCopyright(Canvas canvas) {
+		Paint paint = new Paint();
+		paint.setColor(Color.BLACK);
+		paint.setTypeface(Typeface.DEFAULT_BOLD);
+		paint.setTextSize(16 * displayWidth / 480);
+		Resources resource = getResources();
+		int x = 240 * displayWidth / 480;
+		int y = 600 * displayWidth / 480;
+		canvas.drawText(resource.getString(R.string.KumamonCopyright), x, y, paint);
+	}
+
+	private void getForecast() {
+		try {
+			SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+			if(sharedPreferences.getBoolean("forecast", false)) {
+				Calendar nowDate = Calendar.getInstance();
+				Calendar lastUpdate = Calendar.getInstance();
+				lastUpdate.setTimeInMillis(sharedPreferences.getLong(ForecastTask.KEY_LASTUPDATE, nowDate.getTimeInMillis()));
+				int locateId = Integer.parseInt(sharedPreferences.getString("locate", "63"));
+				if(mLocateId == locateId) {
+					if(lastUpdate.get(Calendar.DAY_OF_YEAR) == nowDate.get(Calendar.DAY_OF_YEAR) &&
+							lastUpdate.get(Calendar.HOUR_OF_DAY) >= 6 && nowDate.get(Calendar.HOUR_OF_DAY) < 12) return;
+					if(lastUpdate.get(Calendar.DAY_OF_YEAR) == nowDate.get(Calendar.DAY_OF_YEAR) &&
+							lastUpdate.get(Calendar.HOUR_OF_DAY) >= 12 && nowDate.get(Calendar.HOUR_OF_DAY) < 18) return;
+					if(lastUpdate.get(Calendar.DAY_OF_YEAR) == nowDate.get(Calendar.DAY_OF_YEAR) &&
+							lastUpdate.get(Calendar.HOUR_OF_DAY) >= 18) return;
+					if(lastUpdate.get(Calendar.DAY_OF_YEAR) == nowDate.get(Calendar.DAY_OF_YEAR) &&
+							lastUpdate.get(Calendar.HOUR_OF_DAY) < 6 && nowDate.get(Calendar.HOUR_OF_DAY) < 6) return;
+				}
+				mLocateId = locateId;
+				ForecastTask task = new ForecastTask(this);
+				task.execute(locateId);
+			}
+		} catch (Exception e) {
+			//ExceptionLog.Log(TAG, e);
+		}
+	}
+}
